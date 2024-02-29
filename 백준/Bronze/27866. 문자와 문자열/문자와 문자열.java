6440242562452306fca4a1d16e72ed8578f6f480
CaFFeiNe <114382361+caffeine0824@users.noreply.github.com>
2024-02-29 18:45:55 +0900

    [Bronze V] Title: 문자와 문자열, Time: 128 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String input = br.readLine();
+		int index = Integer.parseInt(br.readLine());
+		char output = input.charAt(index - 1);
+		bw.write(output);
+		bw.flush();
+	}		
+}
