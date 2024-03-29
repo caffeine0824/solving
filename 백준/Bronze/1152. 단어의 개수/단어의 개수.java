ab8143db0a0ee0fe73bb6f2bdf262e2d0391966a
CaFFeiNe <114382361+caffeine0824@users.noreply.github.com>
2024-03-30 07:35:39 +0900

    [Bronze II] Title: 단어의 개수, Time: 272 ms, Memory: 20108 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		bw.write(st.countTokens() + "");
+		bw.flush();
+	}	
+}
