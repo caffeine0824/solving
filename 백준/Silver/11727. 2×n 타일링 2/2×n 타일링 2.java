e718fa55c7d66a7d47485b0f45699d2bdd4175a3
CaFFeiNe <114382361+caffeine0824@users.noreply.github.com>
2024-04-11 21:45:21 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 144 ms, Memory: 15944 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N, a1, a2, tmp, mod;
+		
+		N = Integer.parseInt(br.readLine());
+		a1 = 0;
+		a2 = 1;
+		for(int i = 0; i < N; i++) {
+			tmp = a2;
+			a2 += a1 * 2 + 10007;
+			a1 = tmp;
+			a2 %= 10007;
+		}
+		bw.write(a2 + "\n");
+		bw.flush();
+	}	
+}
