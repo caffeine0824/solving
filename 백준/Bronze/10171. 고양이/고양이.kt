cf6c1cdb75c21d5ece951a6e3322131a2e2f4576
CaFFeiNe <114382361+caffeine0824@users.noreply.github.com>
2024-04-23 08:10:06 +0900

    [Bronze V] Title: 고양이, Time: 80 ms, Memory: 12132 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.kt
@@ -0,0 +1,20 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.time.LocalDate
+import java.util.Arrays
+import java.util.StringTokenizer
+
+fun main(args: Array<String>) {
+    var br = BufferedReader(InputStreamReader(System.`in`))
+    var bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    bw.write("""
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+    """.trimIndent())
+    bw.flush()
+}
